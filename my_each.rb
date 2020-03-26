366f8498d6b28a0f7551cf46adda4ab86773522b
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 03:12:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,11 @@
-numbers = %w[1 2 3 4 ]
+numbers = %w[1 2 3 4]
 def my_each(numbers) # put argument(s) here
-    if block_given?
    i = 0
   while i > numbers.length
     yield numbers[i]
     i = i +1
   end
-end  
+ 
 numbers
 
 end
